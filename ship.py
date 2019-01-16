88f0a08cd70b3112842ac08cdd378abe9e5fdc94
Jamiu Olashile Salimon <salimonjamiu@yahoo.com>
2019-01-16 14:51:04 +0100

    Fix bug: Initialize the settings instance in Ship() class

--- ship.py
+++ ship.py
@@ -6,7 +6,9 @@ class Ship():
     '''
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
+        #initialize the parameters
         self.screen = screen
+        self.ai_settings = ai_settings
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
